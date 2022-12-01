2f06bdfd675fde10beed1becdfc49414e7f50793
Mikole Rosales <rosalesmikole@gmail.com>
2022-12-01 18:03:13 +0800

    asd

--- team_m3da1_project/team_m3da1_project/settings.py
+++ team_m3da1_project/team_m3da1_project/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'team_m3da1_project.urls'
@@ -135,6 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR / "staticfiles"
 # note: specify static directory location
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), ) 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
